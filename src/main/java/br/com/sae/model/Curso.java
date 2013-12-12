e79ae704ce9e4214106b4ce5f4b6517aad67dce7
Charlles Henrique <charlleshenriquepaiva@hotmail.com>
2013-12-12 20:57:45 -0200

    Criação da classe Curso

---
+++ src/main/java/br/com/sae/model/Curso.java
@@ -0,0 +1,28 @@
+package br.com.sae.model;
+
+import java.io.Serializable;
+
+public class Curso implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long codigo;
+	private String nome;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
